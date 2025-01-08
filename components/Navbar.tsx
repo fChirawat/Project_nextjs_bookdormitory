2031e2edcf3d6bb454288c3e0e6ef894b90e40e3
MangPong <mangpong.eiei@gmail.com>
2025-01-08 23:08:30 +0700

    navV.2

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,28 @@
 export default function Navbar(){
     return(
-         <nav>
-            <div><h1>MangPong</h1>
-            </div>
-            
-    
-         </nav>
+<nav class="bg-pink-200 border-gray-200 ">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a class="flex items-center space-x-3 rtl:space-x-reverse">
+    <img src="/five-stars.png" alt="Five Stars" class="w-48 h-12 object-contain" />
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">OUR PROJECT</span>
+    </a>
+
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-pink-200 ">
+        <li>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0 dark:text-black md:dark:hover:text-red-500  dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0 dark:text-black md:dark:hover:text-red-500  dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0 dark:text-black md:dark:hover:text-red-500  dark:hover:text-white md:dark:hover:bg-transparent">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
        
     )
 }
