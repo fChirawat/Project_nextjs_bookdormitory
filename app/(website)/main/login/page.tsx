12b6589410cec37dec57b2b13f19cc74181ba294
fChirawat <kang2499k11@gmail.com>
2025-01-11 20:02:11 +0700

    project1

--- app/(website)/main/login/page.tsx
+++ app/(website)/main/login/page.tsx
@@ -1,9 +1,10 @@
+import Navbar from '../../../../components/Navbar';
+
 export default function Login() {
-  return (
-    <>
-    <div>
-      <h5>Login ของ คนซื้อ</h5>
-    </div>
-    </>
-  );
+    return (
+        <div>
+            <Navbar />
+            <h5>Login ของ คนซื้อ</h5>
+        </div>
+    );
 }
