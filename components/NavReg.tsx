175bd82d86d63441481bff14b341b246c92f8504
jchukkarin <jchukkarin@gmail.com>
2025-02-13 01:17:09 +0700

    Save local changes before pulling

--- components/NavReg.tsx
+++ components/NavReg.tsx
@@ -1,6 +1,9 @@
+'use client'
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function NavReg() {
+  const router = useRouter(); // ✅ เรียกใช้ useRouter() ในคอมโพเนนต์
   return (
     <nav className="bg-pink-200 border-gray-200">
       <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
@@ -19,6 +22,14 @@ export default function NavReg() {
             Register
           </Link>
         </div>
+
+          {/* ปุ่มย้อนกลับอยู่ด้านขวา */}
+          <button
+          onClick={() => router.push("/main/login")}
+          className="bg-gradient-to-r from-gray-600 to-gray-800 text-white px-6 py-2 rounded-xl shadow-lg hover:from-gray-500 hover:to-gray-700 hover:shadow-xl transition-all duration-300 transform hover:scale-105 active:scale-95"
+        >
+          Go Back
+        </button>
       </div>
     </nav>
   );
--- components/Navragsell.tsx
+++ components/Navragsell.tsx
@@ -1,6 +1,9 @@
+'use client'
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Navregsell() {
+  const router = useRouter();
     return (
       <nav className="bg-pink-200 border-gray-200">
         <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
@@ -15,6 +18,13 @@ export default function Navregsell() {
               Register Affiliate
             </Link>
           </div>
+          {/* ปุ่มย้อนกลับอยู่ด้านขวา */}
+          <button
+          onClick={() => router.push("/main/loginsell")}
+          className="bg-gradient-to-r from-gray-600 to-gray-800 text-white px-6 py-2 rounded-xl shadow-lg hover:from-gray-500 hover:to-gray-700 hover:shadow-xl transition-all duration-300 transform hover:scale-105 active:scale-95"
+        >
+          Go Back
+        </button>
         </div>
       </nav>
     );
