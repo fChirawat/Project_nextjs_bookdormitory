5f9dd751cd5042f4423a528ee435bfaa15fa4401
fChirawat <kang2499k11@gmail.com>
2025-03-01 08:54:15 +0700

    update edit error all

--- prisma/prisma/migrations/20250301015143_init/migration.sql
+++ prisma/prisma/migrations/20250301015143_init/migration.sql
@@ -5,6 +5,7 @@ CREATE TABLE `User` (
     `email` VARCHAR(191) NOT NULL,
     `phone` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
+    `profileId` INTEGER NULL,
 
     UNIQUE INDEX `User_username_key`(`username`),
     UNIQUE INDEX `User_email_key`(`email`),
@@ -19,10 +20,15 @@ CREATE TABLE `Profile` (
     `userId` INTEGER NOT NULL,
     `firstName` VARCHAR(191) NOT NULL,
     `lastName` VARCHAR(191) NOT NULL,
-    `address` VARCHAR(191) NOT NULL,
     `photoIDcard` VARCHAR(191) NOT NULL,
     `relationship` VARCHAR(191) NOT NULL,
     `phoneRelationship` VARCHAR(191) NOT NULL,
+    `phoneNumber` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `contactInfo` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `Profile_userId_key`(`userId`),
     PRIMARY KEY (`id`)
@@ -46,13 +52,23 @@ CREATE TABLE `UserSell` (
 -- CreateTable
 CREATE TABLE `ProfileSell` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `userSellId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `title` VARCHAR(191) NULL DEFAULT '',
     `firstName` VARCHAR(191) NOT NULL,
     `lastName` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NULL DEFAULT '',
+    `phoneNumber` VARCHAR(191) NULL DEFAULT '',
+    `email` VARCHAR(191) NULL DEFAULT '',
     `address` VARCHAR(191) NOT NULL,
-    `photoIDcard` VARCHAR(191) NOT NULL,
+    `bank` VARCHAR(191) NULL DEFAULT '',
+    `accountNumber` VARCHAR(191) NULL DEFAULT '',
+    `profileImage` VARCHAR(191) NULL DEFAULT '',
+    `photoIdCard` VARCHAR(191) NOT NULL,
+    `status` VARCHAR(191) NOT NULL DEFAULT 'pending',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `ProfileSell_userSellId_key`(`userSellId`),
+    UNIQUE INDEX `ProfileSell_userId_key`(`userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -63,17 +79,17 @@ CREATE TABLE `FormSell` (
     `nameDormitory` VARCHAR(191) NOT NULL,
     `typeDormitory` VARCHAR(191) NOT NULL,
     `addressDormitory` VARCHAR(191) NOT NULL,
-    `dormitoryDetails` VARCHAR(191) NOT NULL,
+    `dormitoryDetails` VARCHAR(191) NULL,
     `facilities` VARCHAR(191) NOT NULL,
     `roomNumber` INTEGER NOT NULL,
-    `distanceDormitory` VARCHAR(191) NOT NULL,
+    `distanceDormitory` DOUBLE NOT NULL,
     `roomDeposit` DOUBLE NOT NULL,
     `priceMonth` DOUBLE NOT NULL,
     `priceElectricity` DOUBLE NOT NULL,
     `priceWater` DOUBLE NOT NULL,
     `priceWifi` DOUBLE NOT NULL,
     `priceOther` DOUBLE NOT NULL,
-    `photoDormitory` VARCHAR(191) NOT NULL,
+    `photoDormitory` VARCHAR(191) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -82,7 +98,7 @@ CREATE TABLE `FormSell` (
 ALTER TABLE `Profile` ADD CONSTRAINT `Profile_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `ProfileSell` ADD CONSTRAINT `ProfileSell_userSellId_fkey` FOREIGN KEY (`userSellId`) REFERENCES `UserSell`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `ProfileSell` ADD CONSTRAINT `ProfileSell_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `UserSell`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `FormSell` ADD CONSTRAINT `FormSell_userSellId_fkey` FOREIGN KEY (`userSellId`) REFERENCES `UserSell`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
